e2d552cea85c89afc7267c8b0fd5d15781a92cfa
poopgiggle <poopgiggle@gmail.com>
2014-10-14 19:25:48 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+"use strict"
+jQuery(document).ready(function () {
+
+	$('button').on('click',function(){
+		
+		//get the new search text
+		var find_query = encodeURIComponent($('input').val());
+		
+		//clear the list
+		$('ul').html("");
+		
+		//search twitter
+		$.getJSON("http://hack-lab.herokuapp.com/find?text="+find_query,function(data){
+			// success!! 
+			$.each(data.statuses,function(index,value){
+				
+				//add the tweet to the ul
+				$('ul').append($('<li>').text(value.text));
+
+			});
+			
+			//need to tell jquerymobile to refresh
+			$('ul').listview('refresh').trigger("create");
+		});
+		
+	})
+
+});
